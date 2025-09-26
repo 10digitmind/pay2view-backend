5287154d8358d5b121bdfe73de2dc30d3198a847
Your Name <olubodekehinde2019@gmail.com>
2025-09-26 18:16:48 +0100

    latest

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const groute = require('../backend/Route/Groutes')
+const groute = require('./Route/Groutes')
 const PORT = 5000;
 require('dotenv').config();
 
